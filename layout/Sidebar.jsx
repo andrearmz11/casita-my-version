9ed8427cccd71cba7b30c8e19e9f49c39403795e
andrearmz11 <anddyrmzrgl@gmail.com>
2026-04-25 21:58:09 -0700

    Create Sidebar component for client tab sidebar

---
+++ layout/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <div className="sidebar">
+            <h2>Client Tab</h2>
+            {/* Add sidebar items here */}
+        </div>
+    );
+};
+
+export default Sidebar;
