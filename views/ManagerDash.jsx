8e810296e189b3e21b5e69a8f095fadb872c27ae
andrearmz11 <anddyrmzrgl@gmail.com>
2026-04-25 21:58:54 -0700

    Create ManagerDash component stub

---
+++ views/ManagerDash.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ManagerDash = () => {
+    return (
+        <div>
+            <h1>Manager Dashboard</h1>
+            <p>This is a basic stub for the Manager Dashboard component.</p>
+        </div>
+    );
+};
+
+export default ManagerDash;
