464bfc7e3b72805d5d9fe72b78d1e95674430e14
andrearmz11 <anddyrmzrgl@gmail.com>
2026-04-25 22:00:21 -0700

    Add basic stub component for WelcomeModal

---
+++ modals/WelcomeModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WelcomeModal = () => {
+    return (
+        <div>
+            <h1>Welcome to Casita!</h1>
+            <p>This is a basic welcome message modal.</p>
+        </div>
+    );
+};
+
+export default WelcomeModal;
